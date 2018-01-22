fb4a29731008c793eb9e85bfa30437ddb8f41f61
danhagg <danielhaggerty1976@gmail.com>
2018-01-21 22:08:00 -0600

    tweaks

---
+++ deck.py
@@ -0,0 +1,28 @@
+import collections
+import random
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades hearts diamonds clubs'.split()
+
+    def __init__(self):
+        self._cards = [
+            Card(rank, suit) for suit in self.suits for rank in self.ranks
+        ]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+random.choice(deck)
+random.shuffle(deck)
+
+# To run from command line, from MY user directory
+# exec(open("./Documents/seven_stud/deck.py").read(), globals())
+# b
